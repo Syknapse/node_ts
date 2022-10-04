970060fb897df5ddde974f1cb17a345498fea0c6
Syknapse <29199184+Syknapse@users.noreply.github.com>
2022-10-04 17:25:59 +0200

    Make todo item responsive

--- src/client/components/TodoItem/todoItem.tsx
+++ src/client/components/TodoItem/todoItem.tsx
@@ -25,25 +25,29 @@ const TodoItem: React.FC<ITodoItemProps> = ({
 }) => {
   return (
     <li className={clsx('todo-item', completed && 'completed')}>
-      <Button
-        className={clsx('todo-button', 'completed-button')}
-        isRound
-        onClick={() => onToggleCompleteness({ id, text, dateCreated, completed })}
-      >
-        {completed ? <RecFilled className="todo-icon" /> : <RecOutlined className="todo-icon" />}
-      </Button>
-      <p className="text">{text}</p>
-      <Button
-        className="todo-button"
-        disabled={completed}
-        isRound
-        onClick={() => onEdit({ id, text, dateCreated, completed })}
-      >
-        <Edit className="todo-icon" />
-      </Button>
-      <Button className="todo-button" isRound onClick={() => onDelete({ id, text, dateCreated, completed })}>
-        <Cross className="todo-icon" />
-      </Button>
+      <div className="item-container">
+        <Button
+          className={clsx('todo-button', 'completed-button')}
+          isRound
+          onClick={() => onToggleCompleteness({ id, text, dateCreated, completed })}
+        >
+          {completed ? <RecFilled className="todo-icon" /> : <RecOutlined className="todo-icon" />}
+        </Button>
+        <p className="text">{text}</p>
+      </div>
+      <div className="button-container">
+        <Button
+          className="todo-button"
+          disabled={completed}
+          isRound
+          onClick={() => onEdit({ id, text, dateCreated, completed })}
+        >
+          <Edit className="todo-icon" />
+        </Button>
+        <Button className="todo-button" isRound onClick={() => onDelete({ id, text, dateCreated, completed })}>
+          <Cross className="todo-icon" />
+        </Button>
+      </div>
     </li>
   )
 }
